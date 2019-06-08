e5813d2b90862ea120b4ad890f0e784616a082bb
Alexander Sovpel <alexander.sovpel@gmail.com>
2019-06-08 10:44:37 +0300

    feat: Add report creator form

--- src/components/ReportCreator.jsx
+++ src/components/ReportCreator.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Formik } from 'formik';
 
+import styled from 'styled-components';
 import DetailsContainer from './DetailsContainer';
 
 const TitleEditor = styled.input`
@@ -8,16 +9,52 @@ const TitleEditor = styled.input`
   display: block;
   border: none;
   background: transparent;
-  padding: 2px 4px;
-  font-size: 1.5rem;
+  padding: 0px;
+  margin: 1rem 0;
+  font-size: 1.25rem;
   font-weight: 600;
+  font-family: inherit;
 `;
+const ContentEditor = styled.textarea`
+  width: 100%;
+  height: 90vh;
+  display: block;
+  border: none;
+  background: transparent;
+  padding: 0px;
+  font-size: 1rem;
+  font-family: inherit;
+  resize: none;
+`;
+
+const initialValues = {
+  title: '',
+  content: '',
+};
 
 function ReportCreator(props) {
   return (
     <DetailsContainer>
-      <TitleEditor />
-      <textarea></textarea>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={console.log}
+      >
+        { formProps => (
+          <form onSubmit={formProps.handleSubmit}>
+            <TitleEditor
+              name="title"
+              value={formProps.values.title}
+              onChange={formProps.handleChange}
+              autoFocus={true}
+            />
+            <ContentEditor
+              name="content"
+              value={formProps.values.content}
+              onChange={formProps.handleChange}
+            />
+          </form>
+        ) }
+      </Formik>
     </DetailsContainer>
   );
 }
